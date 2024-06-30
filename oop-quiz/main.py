ea4564c7c32032c8bde19ebaa264a81dff563eae
YunJae Jang <janggoons@gmail.com>
2024-06-30 18:47:59 +0900

    oop-quiz updated

---
+++ oop-quiz/main.py
@@ -0,0 +1,19 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("---------------------------------")
+print("모든 문제를 풀었습니다!")
+print(f"최종 점수는 {quiz.score}/{quiz.question_number} 입니다.")
